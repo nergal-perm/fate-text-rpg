c8ae3d562f698e77665b46c39c75d22bf4dfdda1
nergal-perm <nergal@psu.ru>
2018-06-25 17:16:03 +0500

    Sample kotlin tests

--- kotlin/src/main/kotlin/ru/terekhov/fate/core/Game.kt
+++ kotlin/src/main/kotlin/ru/terekhov/fate/core/Game.kt
@@ -1,5 +1,15 @@
 package ru.terekhov.fate.core
 
+import ru.terekhov.fate.core.locations.Location
+import ru.terekhov.fate.core.locations.LocationLoader
+
 class Game {
+    var currentLocation: Location? = null
+    lateinit var locationLoader: LocationLoader
+
+
+    fun loadLocation(location: Int) {
+        currentLocation = locationLoader.loadLocation(location)
+    }
 
 }
--- kotlin/src/test/kotlin/ru/terekhov/fate/core/GameTest.kt
+++ kotlin/src/test/kotlin/ru/terekhov/fate/core/GameTest.kt
@@ -1,10 +1,36 @@
 package ru.terekhov.fate.core
 
+import org.assertj.core.api.Assertions.*
+
 import org.junit.jupiter.api.Test
+import ru.terekhov.fate.core.locations.Location
+import ru.terekhov.fate.core.locations.LocationLoader
 
 class GameTest {
     @Test
-    fun `some test function`() {
+    fun `should load current location`() {
+        // Given
+        val game = Game()
+        game.locationLoader = StubLocationLoader()
+
+
+        game.loadLocation(1)
+
+        var expectedLocation = Location(1, "Вы пришли на базар")
+        assertThat(game.currentLocation).isEqualTo(expectedLocation)
+
+        game.loadLocation(5)
+        expectedLocation = Location(5, "Всем привет")
+        assertThat(game.currentLocation).isEqualTo(expectedLocation)
+    }
+
+    class StubLocationLoader: LocationLoader {
+        var locations = mapOf<Int, Location>(
+                1 to Location(1, "Вы пришли на базар"),
+                5 to Location(5, "Всем привет"))
+        override fun loadLocation(locationId: Int): Location? {
+            return locations[locationId]
+        }
 
     }
 }
