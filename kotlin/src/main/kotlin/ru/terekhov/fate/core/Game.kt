d2572a92470b07e8b226c58fb3814d8109f4f8b5
nergal-perm <nergal@psu.ru>
2018-06-25 16:10:56 +0500

    Initial Kotlin project structure

---
+++ kotlin/src/main/kotlin/ru/terekhov/fate/core/Game.kt
@@ -0,0 +1,5 @@
+package ru.terekhov.fate.core
+
+class Game {
+
+}
---
+++ kotlin/src/test/kotlin/ru/terekhov/fate/core/GameTest.kt
@@ -0,0 +1,10 @@
+package ru.terekhov.fate.core
+
+import org.junit.jupiter.api.Test
+
+class GameTest {
+    @Test
+    fun `some test function`() {
+
+    }
+}
